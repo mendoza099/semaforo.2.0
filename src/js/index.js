b8824e0381a5f7f8c9f1aa9970771a5625f04c3f
mendoza099 <sergiomendozadominguez@gmail.com>
2022-08-16 09:17:36 +0000

    semaforo

--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// include your styles into the webpack bundle
-import "../styles/index.css";
+//include bootstrap npm library into the bundle
+import "bootstrap";
+
+//include your index.scss file into the bundle
+import 'relative/path/to/stylesheet.scss';
 
 //import your own components
-import Home from "./component/home.jsx";
+import TrafficLight from "./component/trafficLight.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<TrafficLight />, document.querySelector("#app"));
